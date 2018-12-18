b973974195ee7a8af1d405719ae3a1dc75cf4897
Martin Liboswar <m.liboswar.bitbird@outlook.com>
2018-12-18 17:13:33 +0100

    Version 1.0.15

--- index.js
+++ index.js
@@ -2,14 +2,25 @@
 
 module.exports = {
   name: 'bitbird-core-ember-ui',
-  included: function(app) {
+  included: function(parent) {
     //app.import('addon/styles/style.css');
 
     //app.import('node_modules/kalel/kalEl/kalEl.js');
     //app.import('node_modules/kalel/kalEl/cal.css');
+      this._super.included.apply(this, arguments);
 
-    //app.import('node_modules/jquery-ui/ui/disable-selection.js');
-    //app.import('node_modules/jquery-ui/ui/widgets/resizable.js');
-    //app.import('node_modules/jquery-ui/themes/base/resizable.css');
+      //this.import('node_modules/jquery-ui/ui/core.js');
+
+      this.import('node_modules/jquery-ui/ui/version.js');
+      this.import('node_modules/jquery-ui/ui/data.js');
+      this.import('node_modules/jquery-ui/ui/ie.js');
+      this.import('node_modules/jquery-ui/ui/safe-active-element.js');
+      this.import('node_modules/jquery-ui/ui/safe-blur.js');
+      this.import('node_modules/jquery-ui/ui/scroll-parent.js');
+      this.import('node_modules/jquery-ui/ui/plugin.js');
+      this.import('node_modules/jquery-ui/ui/widget.js');
+      this.import('node_modules/jquery-ui/ui/widgets/mouse.js');
+      this.import('node_modules/jquery-ui/ui/widgets/draggable.js');
+      this.import('node_modules/jquery-ui/ui/widgets/sortable.js');
   }
 };

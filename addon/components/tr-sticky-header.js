c3ebf3f3cb027bbd636a1fa9640f3c19e8532899
Martin Liboswar <m.liboswar@bitbird.at>
2019-01-04 08:15:37 +0100

    Fixes image deletion in sticky header

--- addon/components/tr-sticky-header.js
+++ addon/components/tr-sticky-header.js
@@ -13,6 +13,7 @@ export default Component.extend({
     imagePlaceholder: null,
     showImage: false,
     showImageUpload: false,
+    showImageDelete: false,
     showImagePlaceholder: true,
 
     onUpload: null,

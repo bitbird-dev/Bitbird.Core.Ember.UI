b3a41ef25992fd1403a1e236a48c59be6ed05726
Martin Liboswar <m.liboswar.bitbird@outlook.com>
2019-01-11 15:15:12 +0100

    Version 1.0.24 Fixes promise handling issues with select editor items

--- addon/components/tr-select-editor-item.js
+++ addon/components/tr-select-editor-item.js
@@ -2,33 +2,64 @@
 import { observer, computed } from '@ember/object'
 import Component from '@ember/component';
 import layout from '../templates/components/tr-select-editor-item';
+import { isNone } from '@ember/utils';
 const {RSVP: {Promise}} = Ember;
 const { next } = Ember.run;
 
 export default Component.extend({
     layout,
 
+    /**
+     * The source object to take data from
+     */
     content: null,
+
+    /**
+     * Name of the source object property to display
+     */
     valueProperty: null,
 
-    _updateValue: observer('content[isLoaded,isLoading]', 'valueProperty', function() {
+    /**
+     * Defines if the is loading
+     */
+    isLoading: false,
+
+    /**
+     * Holds the raw value from the source object property
+     */
+    rawValue: null,
+
+    /**
+     * Computes the final value to display. Supports promises.
+     */
+    value: computed('rawValue', 'rawValue.content','rawValue.isPending', function() {
+        let rawValue = this.get('rawValue'),
+            rawValueContent = isNone(rawValue) || isNone(rawValue.get) ? null : rawValue.get('content');
+
+        return isNone(rawValueContent) ? rawValue : rawValueContent;
+    }),
+
+    /**
+     * Reads the valueProperty from the content object and sets the rawValue
+     */
+    _updateValue: observer('content', 'content.isLoading', 'content.isLoaded', 'content.isPending', 'valueProperty', function() {
         let valueProperty = this.get('valueProperty'),
             content = this.get('content');
 
         if(!content) {
-            this.set('value', null);
+            this.set('rawValue', undefined);
             return;
         }
 
         if(!content.get) {
-            this.set('value', content);
+            this.set('rawValue', content);
             return;
         }
 
         let value = content.get(valueProperty),
             self = this;
 
-        if(value.then) {
+        if(value && value.then) {
             self.set('isLoading', true);
             value.then(() => {
                 self.set('isLoading', false);
@@ -37,9 +68,6 @@ export default Component.extend({
             });
         }
 
-        this.set('value', value);
+        this.set('rawValue', value);
     }).on('init'),
-
-    isLoading: false,
-    value: null
 });

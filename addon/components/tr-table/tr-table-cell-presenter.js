f00c1694e6bb455c16f4fa2ee8678cc575bfeb36
f.kreuzer@bitbird.at <f.kreuzer@bitbird.at>
2019-02-07 15:04:15 +0100

    improved table data loading

--- addon/components/tr-table/tr-table-cell-presenter.js
+++ addon/components/tr-table/tr-table-cell-presenter.js
@@ -1,6 +1,5 @@
 import Component from '@ember/component';
 import layout from '../../templates/components/tr-table/tr-table-cell-presenter';
-import { observer } from '@ember/object';
 
 /**
  * Represents the state of a table cell.
@@ -8,55 +7,14 @@ import { observer } from '@ember/object';
  * @property  {null} column the column which contains the cell
  */
 export default Component.extend({
-  layout,
-  classNames: "cell-presenter",
+    layout,
+    classNames: "cell-presenter",
 
-  rowValue: null,
-  value: null,
-  column: null,
-  isloading: false,
+    rowValue: null,
+    value: null,
+    column: null,
 
-  valueObserver: observer('rowValue', ()=>this._refreshValue()),
-
-  init(){
-    this._super(...arguments);
-    this._refreshValue();
-  },
-
-  _refreshValue(){
-    // column = 'address.street';
-    let paths = this.column.split('.');
-    let result = this.rowValue.get(paths[0]);
-
-    if(result === undefined){return;}
-
-    if (paths.length == 1) {
-      // synchronous (value is already stored in model, e.g. flat properties like "nr")
-      //console.log(this.column + "; path: [" + 0 + "]: " + JSON.stringify(result));
-      this.value = result;
-      return;
-    }
-
-    this.set('isloading', true);
-    this.set('value', null);
-
-    //async (value may be fetched. promise is returned. wait for promise to resolve by chaining "then").
-
-    for(let i = 1; i < paths.length; i++){
-      result = result.then(x => {
-        //console.log(this.column + "; path: [" + (i-1) + "]: " + JSON.stringify(x));
-        return x.get(paths[i])
-      });
-    }
-    result.then(x => {
-      //console.log(this.column + "; path: [last]: " + JSON.stringify(x));
-      this.set('value', x);
-      this.set('isloading', false);
-    });
-
-    //this.value = result;
-    //result = result.then(x => this.value = x);
-
-    //this.value = result.resolve();
-  }
+    init(){
+      this._super(...arguments);
+    },
 });

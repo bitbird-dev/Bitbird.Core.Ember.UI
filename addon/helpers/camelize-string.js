1ffea9f77c55b03e59ce8fc5364a56b1964da1fa
Martin Liboswar <m.liboswar.bitbird@outlook.com>
2019-02-22 13:09:35 +0100

    Removes camelize-string helper in favor to camelize helper

--- addon/helpers/camelize-string.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-import { camelize } from '@ember/string';
-
-export function camelizeString(params/*, hash*/) {
-  return camelize(params);
-}
-
-export default helper(camelizeString);
